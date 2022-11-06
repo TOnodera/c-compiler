e03450411455b3f6b702e99d69bc11f673590a32
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-11-06 11:06:18 +0000

    足し算と引き算の追加

---
+++ 9cc/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# テスト用シェル関数
+assert(){
+    expected="$1"
+    input="$2"
+
+    ./9cc "$input" > tmp.s
+    cc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expeced, but got $actual"
+        exit 1
+    fi
+}
+
+assert 0 0
+assert 42 42
+assert 21 "5+20-4"
+
+echo OK
