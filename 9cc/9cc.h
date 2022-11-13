60ee0b601ac296528c98a1c4d03395e4c0d3b13e
t.onodera <t.onodera.1.2.1.5@gmail.com>
2022-11-13 17:07:18 +0000

    比較演算子の部分を実装

--- 9cc/9cc.h
+++ 9cc/9cc.h
@@ -21,6 +21,7 @@ struct Token
     Token *next;
     int val;   // kindがNUMの場合、その数値
     char *str; // トークン文字列
+    int len;   // トーケンの
 };
 
 extern Token *token; // 現在着目しているトークン
@@ -35,6 +36,10 @@ typedef enum
     ND_MUL, // *
     ND_DIV, // /
     ND_NUM, // 整数
+    ND_EQ,  // ==
+    ND_NE,  // !=
+    ND_LT,  // <
+    ND_LE,  // <=
 } NodeKind;
 
 struct Node
@@ -47,19 +52,23 @@ struct Node
 
 typedef struct Node Node;
 
-
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node(NodeKind kind);
 Node *new_node_num(int val);
 Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
 Node *primary();
 Node *mul();
 void error(char *fmt, ...);
 void error_at(char *loc, char *fmt, ...);
-bool consume(char op);
-void expect(char op);
+bool consume(char *op);
+void expect(char *op);
 int expect_number();
 bool at_eof();
-Token *new_token(TokenKind kind, Token *cur, char *str);
-Token *tokenize(char *p);
+Token *new_token(TokenKind kind, Token *cur, char *str, int len);
+bool startswith(char *p, char *q);
+Token *tokenize();
 void gen(Node *node);
 Node *unary();
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs);
